e92b491f222a97c9268f6d76ca593a76f319f7e0
Bapusaheb Patil <bapoo1998@gmail.com>
2019-03-23 18:14:00 +0530

    [ADD] Implemented UpdateDayReceiver

--- app/src/main/java/com/bapspatil/moodtracker/UpdateDayReceiver.java
+++ app/src/main/java/com/bapspatil/moodtracker/UpdateDayReceiver.java
@@ -3,11 +3,17 @@ package com.bapspatil.moodtracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class UpdateDayReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 
+        int currentDay = mPreferences.getInt(SharedPreferencesHelper.KEY_CURRENT_DAY, 1);
+        currentDay++;
+        mPreferences.edit().putInt(SharedPreferencesHelper.KEY_CURRENT_DAY, currentDay).apply();
     }
 }
