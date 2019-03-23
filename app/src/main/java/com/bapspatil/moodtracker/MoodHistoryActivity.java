aa7c4228b74e32f423c4434aee0140d6c42a5626
Bapusaheb Patil <bapoo1998@gmail.com>
2019-03-23 14:02:56 +0530

    Refactoring + Code cleanup

--- app/src/main/java/com/bapspatil/moodtracker/MoodHistoryActivity.java
+++ app/src/main/java/com/bapspatil/moodtracker/MoodHistoryActivity.java
@@ -1,7 +1,7 @@
 package com.bapspatil.moodtracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MoodHistoryActivity extends AppCompatActivity {
 
